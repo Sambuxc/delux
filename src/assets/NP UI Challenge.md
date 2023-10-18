54942be8944fa69c6eb461e8b8955bf42e371977
Sam <sam@squashedpixel.co.uk>
2023-10-18 13:17:47 +0100

    base vuejs app structure. Header has been responsive styles

---
+++ src/assets/NP UI Challenge.md
@@ -0,0 +1,30 @@
+#   Rational
+
+Accurately recreate the visuals found: https://www.figma.com/file/MrZR9TZOVrI8pwBjJuDprZ/Code-Review-Template?node-id=1%3A2&mode=dev
+
+The focus should be on the element named "Listings Feed" (found via the left column) which contains the main vehicle cards.
+Consider the design as it changes breakpoint, the site content widths and the responsive nature of the design.
+Whilst there is no need to recreate the other elements on the page (e.g. site header/footer or side filters) consider their positioning and how they interact with your work (TIP: using placeholder elements just to fill those spaces might be a good idea)
+
+
+#   Requirements
+
+Clean, semantic and accessible SCSS and HTML, written to best practices,  ideally project to be built in VueJS/Nuxt.  Assume only the latest browser support required (no IE).
+Use GitHub to track changes and commit your code to.  There is a JSON file with mock vehicle data attached to the email sent which represents typical response data we get from the api.  If you want to use it to populate the cards, please do so.
+
+
+#   Timescales
+
+This is not a timed challenge but please be aware that under normal circumstances work like this would be estimated and resourced accordingly.
+Please try to push complete work within a few days, if you are unable to complete within this time please get in touch and make us aware.
+Don't spend a ton of time on this, we just want to get an impression of how you code and your accuracy.
+
+
+#   Questions
+
+Feel free to contact me with any questions, any time, at rob.dillon@nexuspoint.co.uk
+
+
+#   Completion
+
+If needed please ensure there are adequate instructions to build/run the work. Once complete please email me and we'll organise sharing/feedback etc
---
+++ src/components/Header.vue
@@ -0,0 +1,206 @@
+<script setup>
+
+</script>
+<template>
+  <header>
+    <div class="flex">
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+      
+      <div class="hidden md:flex items-center md:mr-15">
+        <img src="@/assets/star-icon.svg" width="24" height="24" />
+        <span class="md:ml-5">0</span>
+      </div>
+
+      <div class="hidden md:flex items-center">
+        <img src="@/assets/time-icon.svg" timeh="24" height="24" />
+        <span class="md:ml-5">0</span>
+      </div>
+    </div>
+      
+    <div class="flex lg:w-full">
+      <nav class="nav-list">
+        <RouterLink to="/" class="">Home</RouterLink>
+        <RouterLink to="/" class="">Our cars</RouterLink>
+        <RouterLink to="/" class="">Sell your car</RouterLink>
+        <RouterLink to="/" class="">Apply for finance</RouterLink>
+        <RouterLink to="/" class="">About us</RouterLink>
+        <RouterLink to="/" class="">Blog</RouterLink>
+        <RouterLink to="/" class="">Contact</RouterLink>
+      </nav>
+      
+      <nav>
+        <RouterLink to="/" class="btn-blue md:hidden">Contact</RouterLink>
+        <a href="mailto:samwbru@gmail.com" class="btn-blue hidden md:flex order-1 lg:order-2">Email</a>
+        <RouterLink to="/" class="btn-blue hidden md:flex order-2 lg:order-1">Call</RouterLink>
+        <div class="flex lg:hidden">
+          <a class="btn-menu">Menu</a>
+          <img class="hidden md:flex" src="@/assets/menu-icon.svg" alt="menu icon" width="24" height="24" />
+        </div>
+      </nav>
+    </div>
+  </header>
+
+  </template>
+
+  <style scoped>
+  .order-1 {
+    order: 1;
+  }
+
+  .order-2 {
+    order: 2;
+  }
+  
+header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 50px;
+  border-bottom: 1px solid #D1D6E0;
+  padding: 10px 13px;
+}
+
+.hidden {
+  display: none;
+}
+
+.items-center {
+  align-items: center;
+}
+
+.flex {
+  display: flex;
+  align-items: center;
+}
+
+nav {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 13px;
+  width: 100%;
+  text-align: center;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  /*font-family: Overpass;*/
+  line-height: 27px;
+  font-size: 16px;
+  font-weight: 600;
+  color: black;
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+.btn-blue {
+  padding: 11px 15px;
+  height: 30px;
+  background: #82F7FF;
+  border-radius: 16px;
+}
+
+.btn-menu {
+  margin: 0;
+}
+
+.nav-list {
+  display: none;
+}
+
+@media (min-width: 768px) {
+  .md\:mr-15 {
+    margin-right: 15px;
+  }
+  
+  .md\:hidden {
+    display: none;
+  }
+  .md\:flex {
+    display: flex;
+  }
+
+  .md\:ml-5 {
+    margin-left: 5px;
+  }
+
+  header {
+    height: 80px;
+  }
+
+  .btn-blue {
+    padding: 12px 25px;
+    height: 38px;
+  }
+  .btn-menu {
+    margin-left: 30px;
+    margin-right: 10px;
+  }
+
+  .logo {
+    margin-right: 30px;
+  }
+
+  nav {
+    gap: 10px;
+  }
+}
+
+@media (min-width: 1024px) {
+  .lg\:hidden {
+    display: none;
+  }
+
+  .lg\:flex {
+    display: flex;
+  }
+  
+  .lg\:order-1 {
+    order: 1;
+  }
+
+  .lg\:order-2 {
+    order: 2;
+  }
+
+  .lg\:w-full {
+    width: 100%;
+  }
+  
+  header {
+    padding: 30px 32px;
+    height: 100px;
+    display: flex;
+    align-items: center;
+  }
+
+  .logo {
+    margin: 0 32px 0 0;
+    width: 200px;
+  }
+
+  nav {
+    font-size: 100%;
+  }
+
+  .nav-list {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 30px;
+  }
+}
+</style>
