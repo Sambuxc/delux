c9b1cfb880f9b559a39d273bc853ae8b5c7f022c
Sam <sam@squashedpixel.co.uk>
2023-10-25 10:02:30 +0100

    updated for github pages config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // github pages app location
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
